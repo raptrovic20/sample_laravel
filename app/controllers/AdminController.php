846a081f4422b5e25625d29d21738dafe0079a76
Raphael Villanueva <rv.villanueva@truelogic.com.ph>
2016-03-03 16:05:07 +0800

    first commit

---
+++ app/controllers/AdminController.php
@@ -0,0 +1,12 @@
+<?php
+class AdminController extends BaseController {
+    public function index()
+    {
+        die("here");
+    }
+
+    public function add()
+    {
+        die("here");
+    }
+}
